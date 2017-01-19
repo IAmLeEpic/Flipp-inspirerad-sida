2ac6f61380d6bf41578904f349e23c78e3c56a48
IAmLeEpic <te15.jonathan.sjogren@umea.nti.se>
2017-01-19 11:32:06 +0100

    Sidebar tweaks, kod rengöring, experimentering

---
+++ script.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+        
+                
+	/*Menu-toggle*/
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+    });
+        
+        });
